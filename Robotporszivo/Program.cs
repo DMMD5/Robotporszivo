93f899af0192534db1c11761b4468ede24ffbfce
DMMD5 <b3romdor@vasvari.org>
2026-01-04 21:14:06 +0100

    szabad

--- Robotporszivo/Program.cs
+++ Robotporszivo/Program.cs
@@ -17,14 +17,41 @@ namespace Robotporszivo
 
             do
             {
-                Console.Write("N(20-30): ");
+                Console.Write("N (20-30): ");
                 sorok = int.Parse(Console.ReadLine());
-                Console.Write("M(20-30): ");
+                Console.Write("M (20-30): ");
                 oszlopok = int.Parse(Console.ReadLine());
             }
-            while (!(sorok >= 20 && sorok <= 30 && oszlopok >= 20 && oszlopok <= 30));
+            while (!(sorok >= 20 && sorok <= 30 && oszlopok >= 20 && oszlopok <= 30 && sorok != oszlopok));
 
             char[,] terkep = new char[sorok, oszlopok];
+
+            int szabadDb = 0, koszDb = 0;
+            while (szabadDb == 0 || koszDb == 0)
+            {
+                szabadDb = 0;
+                koszDb = 0;
+
+                for (int i = 0; i < sorok; i++)
+                {
+                    for (int j = 0; j < oszlopok; j++)
+                    {
+                        int x = rnd.Next(100);
+                        if (x < 50)
+                        {
+                            terkep[i, j] = '-';
+                            szabadDb++;
+                        }
+                        else if (x < 70)
+                        {
+                            terkep[i, j] = 'B';
+                        }
+                    }
+                }
+
+
+
+            }
         }
     }
 }
